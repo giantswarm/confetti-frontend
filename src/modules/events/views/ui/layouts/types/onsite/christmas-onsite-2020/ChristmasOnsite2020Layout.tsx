3604e8f41e78020bda9c9954c58e12d045cfe7a2
Adrian Barsan <mnlk123@yahoo.com>
2020-11-10 20:49:43 +0100

    Add background for christmas onsite

--- src/modules/events/views/ui/layouts/types/onsite/christmas-onsite-2020/ChristmasOnsite2020Layout.tsx
+++ src/modules/events/views/ui/layouts/types/onsite/christmas-onsite-2020/ChristmasOnsite2020Layout.tsx
@@ -1,14 +1,54 @@
 import { Box } from "grommet";
+import styled from "styled-components";
 
 import { EventLayoutProps } from "../../../layouts";
+import Background from "./background/Background";
+import { christmasOnsite2020Palette } from "./palette";
 
-interface ChristmasOnsite2020Layout extends EventLayoutProps<"onsite"> {}
+const Wrapper = styled(Box)`
+    position: relative;
+    height: 100vh;
+    width: 100%;
+`;
 
-const ChristmasOnsite2020Layout: React.FC<ChristmasOnsite2020Layout> = ({ event, ...rest }) => {
+const Sky = styled(Box)`
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: ${christmasOnsite2020Palette.sky};
+    z-index: 0;
+`;
+
+const Ground = styled(Box)`
+    height: 60%;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: ${christmasOnsite2020Palette.ground};
+    z-index: 1;
+`;
+
+const StyledBackground = styled(Background)`
+    position: absolute;
+    display: block;
+    bottom: calc(60% - 0.8vh);
+    left: 0;
+    width: 100%;
+    z-index: 1;
+`;
+
+interface ChristmasOnsite2020LayoutProps extends EventLayoutProps<"onsite"> {}
+
+const ChristmasOnsite2020Layout: React.FC<ChristmasOnsite2020LayoutProps> = ({ event, ...rest }) => {
     return (
-        <Box {...rest} pad='medium'>
-            Christmas onsite
-        </Box>
+        <Wrapper {...rest}>
+            <Sky />
+            <StyledBackground />
+            <Ground />
+        </Wrapper>
     );
 };
 
---
+++ src/modules/events/views/ui/layouts/types/onsite/christmas-onsite-2020/palette.ts
@@ -0,0 +1,5 @@
+export const christmasOnsite2020Palette = {
+    sky: "#B9DCF9",
+    ground: "#CAEAFC",
+    forest: "#83B5E5",
+};
