4f35aca59ca7d8ae3d846c3fdb8b821ff98e3cff
Adrian Barsan <mnlk123@yahoo.com>
2020-11-11 20:38:05 +0100

    Increase quality of gs image

--- src/modules/events/models/EventsRepository.ts
+++ src/modules/events/models/EventsRepository.ts
@@ -153,7 +153,6 @@ export class EventsRepository extends Repository {
 
     public stopWatchingEvent = (eventID: string): void => {
         this.eventsService.stopWatchingEvent(eventID);
-        if (this.activeOnsiteRoom) this.activeOnsiteRoom.attendeeCounter--;
         this.activeEventID.data = null;
         this.persistingStrategy.delete(EventsRepository.activeEventIDStorageKey);
     };
