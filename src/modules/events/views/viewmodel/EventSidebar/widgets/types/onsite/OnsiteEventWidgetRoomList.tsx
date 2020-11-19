6debec97afc5d08bf9832776a02a253ef6688b11
Gerald Pape <gerald@giantswarm.io>
2020-11-19 12:27:02 +0100

    change look of room list links

--- src/modules/events/views/viewmodel/EventSidebar/widgets/types/onsite/OnsiteEventWidgetRoomList.tsx
+++ src/modules/events/views/viewmodel/EventSidebar/widgets/types/onsite/OnsiteEventWidgetRoomList.tsx
@@ -1,5 +1,4 @@
 import { Anchor, Box, Heading, Text } from "grommet";
-import { FormNextLink } from "grommet-icons";
 import { observer } from "mobx-react-lite";
 
 import { OnsiteEventRoom } from "@/modules/events/models/types/onsite/OnsiteEventRoom";
@@ -19,13 +18,15 @@ const OnsiteEventWidgetRoomList: React.FC<OnsiteEventWidgetPlaceholderProps> = (
                 </Heading>
             </Box>
             {rooms.map((room) => (
-                <Anchor key={room.id} onClick={() => joinRoom(eventID, room.id)}>
-                    <Box direction='row' margin={{ vertical: "none" }} align='center'>
-                        <Text size='medium' margin={{ right: "xsmall", bottom: "none" }}>
-                            {room.name} ({room.attendeeCounter})
-                        </Text>
-                        <FormNextLink />
-                    </Box>
+                <Anchor
+                    key={room.id}
+                    onClick={() => joinRoom(eventID, room.id)}
+                    color='text'
+                    margin={{ bottom: "xsmall" }}
+                >
+                    <Text size='medium'>
+                        {room.name} ({room.attendeeCounter})
+                    </Text>
                 </Anchor>
             ))}
         </Box>
