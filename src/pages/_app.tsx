979298d5c81b55a528e361bf1f4b34f3d40f8d37
Oliver Ponder <oliverponder@giantswarm.io>
2020-12-10 18:56:31 +0800

    Fix typo in other places too

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,13 +39,13 @@ const AppWrapper: React.FC<AppWrapperProps> = ({ Component, pageProps }) => {
 
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://christmas2020.giantswarm.io/events" />
-                <meta property="og:title" content="Hang out at the Giant Swarm’s Remote Christmas Market" />
+                <meta property="og:title" content="Hang out at the Giant Swarm Remote Christmas Market" />
                 <meta property="og:description" content="Say hi at a stall that grabs your interest! You can see how many people are at a stall, and join the conversation directly." />
                 <meta property="og:image" content="/social_share_preview.png" />
 
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://christmas2020.giantswarm.io/events" />
-                <meta property="twitter:title" content="Hang out at the Giant Swarm’s Remote Christmas Market" />
+                <meta property="twitter:title" content="Hang out at the Giant Swarm Remote Christmas Market" />
                 <meta property="twitter:description" content="Say hi at a stall that grabs your interest! You can see how many people are at a stall, and join the conversation directly." />
                 <meta property="twitter:image" content="/social_share_preview.png" />
 
