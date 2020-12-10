290b7ec7d3bdd17a81eb83c078496627238ce452
Oliver Ponder <oliverponder@giantswarm.io>
2020-12-10 18:44:41 +0800

    Social share preview (#46)
    * Add social share preview metadata
    
    * Remove unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,6 +31,24 @@ const AppWrapper: React.FC<AppWrapperProps> = ({ Component, pageProps }) => {
                 <link rel='shortcut icon' type='image/x-icon' href='https://s.giantswarm.io/brand/1/favicon32.ico' />
                 <link rel='icon' sizes='152x152' href='https://s.giantswarm.io/brand/1/icon_152x152.png' />
                 <link rel='apple-touch-icon' sizes='152x152' href='https://s.giantswarm.io/brand/1/icon_152x152.png' />
+
+
+                <title>Giant Swarm Remote Christmas Market</title>
+                <meta name="title" content="Hang out at the Giant Swarm’s Remote Christmas Market" />
+                <meta name="description" content="Say hi at a stall that grabs your interest! You can see how many people are at a stall, and join the conversation directly." />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://christmas2020.giantswarm.io/events" />
+                <meta property="og:title" content="Hang out at the Giant Swarm’s Remote Christmas Market" />
+                <meta property="og:description" content="Say hi at a stall that grabs your interest! You can see how many people are at a stall, and join the conversation directly." />
+                <meta property="og:image" content="/social_share_preview.png" />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://christmas2020.giantswarm.io/events" />
+                <meta property="twitter:title" content="Hang out at the Giant Swarm’s Remote Christmas Market" />
+                <meta property="twitter:description" content="Say hi at a stall that grabs your interest! You can see how many people are at a stall, and join the conversation directly." />
+                <meta property="twitter:image" content="/social_share_preview.png" />
+
                 <script src='/env.js' crossOrigin='anonymous' />
             </Head>
             <GlobalStyle />
